0d4232e8c76d6a851e98d0eef368c5305002dc37
Elyes <elyesmahjoubi@hotmail.com>
2022-02-26 21:23:48 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/elyesmahjoubi.github.io/precache-manifest.61757ca8a9469c5cdc1cb8230f5e19ba.js"
+  "/elyesmahjoubi.github.io/precache-manifest.15c7e1ed8e73774038987c7f138eb5d8.js"
 );
 
 self.addEventListener('message', (event) => {
