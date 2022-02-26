43ea86fdab403a4a1135956ab5108a2b3ae41187
Elyes <elyesmahjoubi@hotmail.com>
2022-02-26 21:11:37 +0100

    Updates

--- precache-manifest.61757ca8a9469c5cdc1cb8230f5e19ba.js
+++ precache-manifest.61757ca8a9469c5cdc1cb8230f5e19ba.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5418fe7f9a2f78622b2ca57a92d25ad7",
+    "revision": "fc8cd0eae39127fb552fa554e44992ff",
     "url": "/elyesmahjoubi.github.io/index.html"
   },
   {
-    "revision": "dba719f78405d5f3be6f",
+    "revision": "e39e38c95c069ebe7cde",
     "url": "/elyesmahjoubi.github.io/static/css/main.810d9e79.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/elyesmahjoubi.github.io/static/js/2.5f88eb19.chunk.js"
   },
   {
-    "revision": "dba719f78405d5f3be6f",
-    "url": "/elyesmahjoubi.github.io/static/js/main.a663a069.chunk.js"
+    "revision": "e39e38c95c069ebe7cde",
+    "url": "/elyesmahjoubi.github.io/static/js/main.55ee590e.chunk.js"
   },
   {
     "revision": "dfd779b5fa4d84bfb617",
