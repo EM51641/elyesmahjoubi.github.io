8b47e611c40bd4226a7e34f3d1678a6c248e5188
Elyes Mahjoubi <Elyes.Mahjoubi@gartner.com>
2024-09-14 19:03:25 +0200

    cleaned and added job descriptions

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,24 +9,22 @@ const settings = {
 const seo = {
   title: "Elyes Mahjoubi",
   description:
-    "I have a great passion for applied mathematics and programming. I currently graduated from HEC Lausanne, where I inherited a strong technical and I am, now, aiming to specialize in mathematical modeling, quantitative research and software engineering.",
+    "I have a great passion for applied mathematics and programming. I graduated from HEC Lausanne, where I developed a strong technical foundation. I am now aiming to specialize in software engineering, with a strong emphasis on scalability.",
   og: {
     title: "Elyes Mahjoubi's Portfolio",
     type: "website",
     url: "https://em51641.github.io",
   },
 };
-
 //Home Page
 const greeting = {
   title: "Elyes Mahjoubi",
   logo_name: "Elyes Mahjoubi",
   nickname: "EM51641",
   subTitle:
-  "I have a great passion for applied mathematics and programming. I currently graduated from HEC Lausanne, where I inherited a strong technical and I am, now, aiming to specialize in mathematical modeling, quantitative research and software engineering.",
-  
-  resumeLink: "https://drive.google.com/open?id=1XYpYhLeqCdyx_q6l0bQoC7RgwQjAjXPf",
-
+    "I have a great passion for applied mathematics and programming. I graduated from HEC Lausanne, where I developed a strong technical foundation. I am now aiming to specialize in software engineering, with a strong emphasis on scalability.",
+  resumeLink:
+    "https://drive.google.com/open?id=1XYpYhLeqCdyx_q6l0bQoC7RgwQjAjXPf",
   portfolio_repository: "https://github.com/EM51641",
 };
 
@@ -60,10 +58,10 @@ const skills = {
       title: "Mathematical Modeling, Finance & Programming",
       fileName: "DataScienceImg",
       skills: [
-        "⚡ Developing stochastic volatility models to evaluate option prices fairly.",
-        "⚡ Experience in creating risk management tools to find portfolios' exposures.",
-        "⚡ Complex quantitative modeling for dynamic forecasting and time series analysis.",
-        "⚡ Mastering a slate of apis' development frameworks to improve data flow."
+        "⚡ Developed stochastic volatility models for fair option pricing.",
+        "⚡ Created risk management tools to assess portfolio exposures.",
+        "⚡ Conducted complex quantitative modeling for dynamic forecasting and time series analysis.",
+        "⚡ Mastered various API development frameworks to enhance data flow.",
       ],
       softwareSkills: [
         {
@@ -71,14 +69,14 @@ const skills = {
           fontAwesomeClassname: "logos:python",
           style: {
             backgroundColor: "transparent",
-          }
+          },
         },
         {
           skillName: "Java",
           fontAwesomeClassname: "logos:java",
           style: {
             backgroundColor: "transparent",
-          }
+          },
         },
         {
           skillName: "C++",
@@ -101,61 +99,72 @@ const skills = {
           style: {
             backgroundColor: "transparent",
             color: "#3776AB",
-          }
+          },
         },
         {
-            skillName: "Javascript",
-            fontAwesomeClassname: "logos:javascript",
-            style: {
-              backgroundColor: "transparent",
-              color: "#3776AB",
-           }
+          skillName: "Javascript",
+          fontAwesomeClassname: "logos:javascript",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
           },
-          {
-            skillName: "Typescript",
-            fontAwesomeClassname: "logos:typescript",
-            style: {
-              backgroundColor: "transparent",
-            }
+        },
+        {
+          skillName: "Typescript",
+          fontAwesomeClassname: "logos:typescript",
+          style: {
+            backgroundColor: "transparent",
           },
-          {
-            skillName: "Docker",
-            fontAwesomeClassname: "logos:docker-icon",
-            style: {
-              backgroundColor: "transparent",
-            }
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "logos:docker-icon",
+          style: {
+            backgroundColor: "transparent",
           },
-          {
-            skillName: "Node.js",
-            fontAwesomeClassname: "logos:nodejs",
-            style: {
-              backgroundColor: "transparent",
-              color: "#3776AB",
-            }},
-          {
-            skillName: "Django Framework",
-            fontAwesomeClassname: "vscode-icons:file-type-django",
-            style: {
-              backgroundColor: "transparent",
-              color: "#3776AB",
-            }},
-          {
-            skillName: "Flask",
-            fontAwesomeClassname: "logos:flask",
-            style: {
-              backgroundColor: "transparent",
-            }},
-            {
-            skillName: "Selenium",
-            fontAwesomeClassname: "logos:selenium",
-            style: {
-              backgroundColor: "transparent",
-              color: "#3776AB",
-            }}
-          ],
+        },
+        {
+          skillName: "Node.js",
+          fontAwesomeClassname: "logos:nodejs",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
+          },
+        },
+        {
+          skillName: "Django Framework",
+          fontAwesomeClassname: "vscode-icons:file-type-django",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
+          },
+        },
+        {
+          skillName: "Flask",
+          fontAwesomeClassname: "logos:flask",
+          style: {
+            backgroundColor: "transparent",
+          },
+        },
+        {
+          skillName: "Phoenix",
+          fontAwesomeClassname: "logos:phoenix",
+          style: {
+            backgroundColor: "transparent",
+          },
+        },
+        {
+          skillName: "Selenium",
+          fontAwesomeClassname: "logos:selenium",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
+          },
         },
       ],
-    }
+    },
+  ],
+};
 
 const competitiveSites = {
   competitiveSites: [],
@@ -170,9 +179,9 @@ const degrees = {
       alt_name: "HEC Lausanne",
       duration: "2019 - 2021",
       descriptions: [
-        "⚡ Emphasised on mathematical finance.",
-        "⚡ Major in asset and risk Management.",
-        "⚡ Academic thesis on the topic of 'Rough Volatility Modeling' in option pricing obtained with a grade of 6.0/6.0.",
+        "⚡ Focused on mathematical finance.",
+        "⚡ Specialized in asset and risk management.",
+        "⚡ Completed an academic thesis on 'Rough Volatility Modeling' in option pricing, achieving a grade of 6.0/6.0.",
       ],
       website_link: "https://www.unil.ch",
     },
@@ -183,17 +192,26 @@ const degrees = {
       alt_name: "HEC Lausanne",
       duration: "2016 - 2019",
       descriptions: [
-        "⚡ Emphasised on macroeconomics, microeconomics and finance.",
-        "⚡ Consolidated my skills in programming, mathematics and statistics.",
-        "⚡ Choose a numerous of research-driven lessons.",
+        "⚡ Focused on macroeconomics, microeconomics, and finance.",
+        "⚡ Strengthened skills in programming, mathematics, and statistics.",
+        "⚡ Selected numerous research-driven courses.",
       ],
       website_link: "https://www.unil.ch",
-    }
+    },
   ],
 };
 
 const certifications = {
   certifications: [
+    {
+      title: "Stochastic Processes",
+      subtitle: "- Vladimir Panov",
+      logo_path: "higher_school_of_econs_logo.svg",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/verify/3DCB689VCWM8?utm_source=ios&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course",
+      alt_name: "Imperial College London",
+      color_code: "#003E74",
+    },
     {
       title: "Mathematics for Machine Learning: Linear Algebra",
       subtitle: "- David Dye",
@@ -227,10 +245,10 @@ const experience = {
           duration: "February - September 2021 ",
           location: "Lausanne, Switzerland",
           description: [
-            "⚡ Compared the efficiency and accuracy of a slate of option pricing models.",
-            "⚡ Modelized volatility surfaces of various models using Gaussian processes.",
-            "⚡ Used machine learning to forecast future market expectations based on the current term structure.",
-            "⚡ Massively consolidated my knowledge of non-Markovian processes.",
+            "⚡ Evaluated the efficiency and accuracy of various option pricing models.",
+            "⚡ Modeled volatility surfaces using Gaussian processes for different financial models.",
+            "⚡ Applied machine learning techniques to forecast future market expectations based on the current term structure.",
+            "⚡ Deepened my understanding of non-Markovian processes.",
           ],
           color: "#0879bf",
         },
@@ -240,14 +258,34 @@ const experience = {
       title: "Work",
       experiences: [
         {
+          title: "Software Engineer",
+          company: "Gartner",
+          company_url: "https://www.gartner.com",
+          logo_path: "gartner_logo.svg",
+          duration: "From January 2024",
+          location: "Barcelona, Spain",
+          description: [
+            "⚡ Enhanced the repository API by applying the Query Specification pattern, resulting in improved query flexibility and maintainability.",
+            "⚡ Expanded the GraphQL schema by incorporating new types and resolvers to support additional critical functionalities, including the ability to display reviews filtered by various facets such as category, industry, and more.",
+            "⚡ Tracked latency in Datadog to identify cache reshuffle alternatives due to slow warm-up times, and generated supporting statistics to back the findings.",
+            "⚡ Developed new domains and services to retrieve data from various sources, including EAVs, traditional RDBMS tables, and persistent in-memory storage.",
+            "⚡ Managed deployments on EKS clusters with Kubernetes, utilizing Helm for package management and the CSI driver for storage provisioning.",
+            "⚡ Oversaw data ingestion processes and the creation/update of Kafka consumers.",
+            "⚡ Extanded my knowledge on RDS, ECR, IAM, Route53, CloudFront, Secret Manager and other AWS services.",
+          ],
+          color: "#0879bf",
+        },
+        {
           title: "Software Developer",
           company: "Amazon",
           company_url: "https://www.amazon.com",
           logo_path: "amazon_logo.svg",
-          duration: "From May 2022 ",
+          duration: "May 2022 - October 2023",
           location: "Iasi, Romania",
           description: [
-            "⚡ Worked on creating internal web APIs for associates and business analysts using Flask and NextJS frameworks, Amazon Redshift and ETL pipelines."          ],
+            "⚡ Built and maintained a REST API in Python (Flask) to extract data from the OLAP Redshift Database, enabling associates to add rules and automatically raise tickets when rules are broken, saving the company over $1M per month.",
+            "⚡ Implemented and maintained a REST API to automate invoice filling for sellers, using PostgreSQL, NestJS, and TypeORM, with a PySimpleGUI interface for associates to commit states temporarily and send forms to the Amazon vendor micro-service, saving the company $500K.",
+          ],
           color: "#0879bf",
         },
         {
@@ -258,14 +296,13 @@ const experience = {
           duration: "August - September 2019",
           location: "Tunis, Tunisia",
           description: [
-            "⚡ Proposed a more realistic metric to analyze the Conditional Value at Risk using a famous research paper (McNeil and Frey, 2000).",
-            "⚡ Set up Python programs to automate the generation of significant financials indexes such as Sharpe ratios and volatility metrics."
+            "⚡ Developed a more realistic metric for analyzing conditional value at risk, inspired by the research of McNeil and Frey (2000).",
+            "⚡ Automated the generation of key financial indexes, including Sharpe ratios and volatility metrics, using Python programs.",
           ],
           color: "#0879bf",
         },
       ],
     },
-
     {
       title: "Volunteerships",
       experiences: [
@@ -277,8 +314,8 @@ const experience = {
           duration: "Janvier 2018 - Fevrier 2019",
           location: "Lausanne, Switzerland",
           description: [
-            "⚡ Supervised participatory conversations between geopolitical experts and students.",
-            "⚡ Supervised a Debate on the 16th October 2018 between two political groups: UDC and PS.",
+            "⚡ Facilitated discussions between geopolitical experts and students.",
+            "⚡ Helped organize a debate on October 16, 2018, between two political groups: UDC and PS.",
           ],
           color: "#4285F4",
         },
@@ -290,8 +327,8 @@ const experience = {
           duration: "February 2019",
           location: "Lausanne, Switzerland",
           description: [
-            "⚡ Represented Luxembourg at the NATO committee.",
-            "⚡ Participated in managing a crisis simulation between the United States of America and Russia.",
+            "⚡ Represented Luxembourg in the NATO committee.",
+            "⚡ Engaged in managing a crisis simulation between the United States and Russia.",
           ],
           color: "#D83B01",
         },
@@ -303,8 +340,8 @@ const experience = {
           duration: "Mars 2018",
           location: "Panama, Panama City",
           description: [
-            "⚡ Represented Colombia at the World Health Organization committee.",
-            "⚡ Helped propose a cooperation campaign across the committee members for the typical treatment of mental illnesses.",
+            "⚡ Represented Colombia in the World Health Organization committee.",
+            "⚡ Contributed to proposing a cooperative campaign among committee members for the standard treatment of mental illnesses.",
           ],
           color: "#000000",
         },
@@ -317,13 +354,13 @@ const experience = {
 const projectsHeader = {
   title: "Projects",
   description:
-    "My work focuses on using various stochastic models to manage portfolios' exposures and estimate options' prices.",
+    "My projects primarily focus on software development, emphasizing clean coding and architecture to counter real world issues. In the past, I created several software solutions that utilize various stochastic models to manage portfolio exposures and estimate option prices.",
   avatar_image_path: "projects_image.svg",
 };
 
 const publicationsHeader = {
   title: "Research",
-  description: "I have worked on several research projects.",
+  description: "I have participated in few research projects.",
   avatar_image_path: "projects_image.svg",
 };
 
@@ -344,7 +381,7 @@ const contactPageData = {
   //},
   addressSection: {
     title: "Email Address",
-    subtitle: "If you have a request about my work, you can email me at:",
+    subtitle: "For inquiries regarding me or my work, please email me at:",
     location_map_link: "mailto:elyesmahjoubi@gmail.com",
     avatar_image_path: "address_image.svg",
   },
